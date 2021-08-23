02196931a4b9332a46e650fc28cc5e3d39d33ff7
ZarckDev <55784207+ZarckDev@users.noreply.github.com>
2021-08-23 12:19:46 +0200

    starting layout

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout';
+
+export default function AboutPage() {
+  return (
+    <Layout title='A propos de DJ Events'>
+      <h1>A propos</h1>
+      <p>
+        Cette application propose les derniers événements de vos DJ préférés et
+        d'autres concerts
+      </p>
+      <p>Version: 1.0.0</p>
+    </Layout>
+  );
+}
---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout';
+
+export default function AddEventPage() {
+  return (
+    <Layout title='Ajouter un événement'>
+      <h1>Add Event</h1>
+    </Layout>
+  );
+}

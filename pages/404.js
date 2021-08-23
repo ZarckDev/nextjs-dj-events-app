5e2bde591d347df054c962f635cfd3e3e83e6ad2
ZarckDev <55784207+ZarckDev@users.noreply.github.com>
2021-08-23 12:30:15 +0200

    Aliases for paths with jsconfig

---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import { FaExclamationTriangle } from 'react-icons/fa';
+import Link from 'next/link';
+import Layout from '@/components/Layout';
+import styles from '@/styles/404.module.css';
+
+export default function NotFoundPage() {
+  return (
+    <Layout title='Page Non trouvée'>
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationTriangle />
+          404
+        </h1>
+        <h4>Désolé, il n'y a rien ici</h4>
+        <Link href='/'>Retour</Link>
+      </div>
+    </Layout>
+  );
+}

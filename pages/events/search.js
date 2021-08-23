60d4b029f234a95d5587693f5f0728d3337dc5c2
ZarckDev <55784207+ZarckDev@users.noreply.github.com>
2021-08-23 19:54:11 +0200

    Add Search bar

---
+++ pages/events/search.js
@@ -0,0 +1,42 @@
+import qs from 'qs';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+import { API_URL } from '@/config/index';
+
+export default function SearchPage({ events }) {
+  const router = useRouter();
+
+  return (
+    <Layout>
+      <h1>Résultats de la recherche pour : {router.query.term}</h1>
+      <Link href='/'>Retour</Link>
+      {events.length === 0 && <h3>Pas de concerts pour l'instant</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ query: { term } }) {
+  const query = qs.stringify({
+    _where: {
+      _or: [
+        { name_contains: term },
+        { performers_contains: term },
+        { description_contains: term },
+        { venue_contains: term },
+      ],
+    },
+  });
+  const res = await fetch(`${API_URL}/events?${query}`);
+  const events = await res.json();
+
+  return {
+    props: {
+      events,
+    },
+  };
+}

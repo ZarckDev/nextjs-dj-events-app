697c7cb10894ae89b20ca8118ad57725efda8a6b
ZarckDev <55784207+ZarckDev@users.noreply.github.com>
2021-08-25 22:07:23 +0200

    Display user dashboard events

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,47 @@
+import { parseCookies } from '@/helpers/index';
 import Layout from '@/components/Layout';
+import DashboardEvent from '@/components/DashboardEvent';
+import { API_URL } from '@/config/index';
+import styles from '@/styles/Dashboard.module.css';
 
-export default function DashboardPage() {
+export default function DashboardPage({ events }) {
+  // console.log(events);
+
+  const deleteEvent = (id) => {
+    console.log(id);
+  };
+
   return (
     <Layout title='Mon compte'>
-      <h1>Mon compte</h1>
+      <div className={styles.dash}>
+        <h1>Mon compte</h1>
+        <h3>Mes événements</h3>
+
+        {events.map((evt) => (
+          <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
+        ))}
+      </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  // console.log(token);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const events = await res.json();
+
+  return {
+    props: {
+      events,
+    },
+  };
 }

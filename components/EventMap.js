c49cab0683028d0933141640a2fca942d9f878af
ZarckDev <55784207+ZarckDev@users.noreply.github.com>
2021-08-26 19:58:38 +0200

    Set Bordeaux as default Mapbox location

--- components/EventMap.js
+++ components/EventMap.js
@@ -10,8 +10,8 @@ export default function EventMap({ evt }) {
   const [loading, setLoading] = useState(true);
 
   const [viewport, setViewport] = useState({
-    latitude: 40.712772,
-    longitude: -73.935242,
+    latitude: 44.8378,
+    longitude: -0.594,
     width: '100%',
     height: '500px',
     zoom: 12,

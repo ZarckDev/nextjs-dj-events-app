382e04c8aaf80f2908f7638da2be8d52779ea659
ZarckDev <55784207+ZarckDev@users.noreply.github.com>
2021-08-26 10:52:19 +0200

    Add Map from Mapbox with Geoapify GeoCoding API, on single event page

---
+++ components/EventMap.js
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+import { NEXT_URL } from '@/config/index';
+import { useState, useEffect } from 'react';
+import ReactMapGl, { Marker } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+export default function EventMap({ evt }) {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const [viewport, setViewport] = useState({
+    latitude: 40.712772,
+    longitude: -73.935242,
+    width: '100%',
+    height: '500px',
+    zoom: 12,
+  });
+
+  useEffect(() => {
+    fetch(
+      `https://api.geoapify.com/v1/geocode/search?text=${evt.address}&apiKey=${process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        const { lat, lon } = result.features[0].properties;
+        setLat(lat);
+        setLng(lon);
+        setViewport({ ...viewport, latitude: lat, longitude: lon });
+        setLoading(false);
+      })
+      .catch((error) => console.log('error', error));
+  }, []);
+
+  if (loading) return false;
+
+  return (
+    //   onViewportChange to be able to move the map zoom in and zoom out
+    <ReactMapGl
+      {...viewport}
+      mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+      onViewportChange={(vp) => setViewport(vp)}
+    >
+      <Marker key={evt.id} latitude={lat} longitude={lng}>
+        <Image src='/images/pin.svg' width={30} height={30} />
+      </Marker>
+    </ReactMapGl>
+  );
+}
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -5,6 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import Layout from '@/components/Layout';
+import EventMap from '@/components/EventMap';
 import { API_URL } from '@/config/index';
 import styles from '@/styles/Event.module.css';
 import { useRouter } from 'next/router';
@@ -36,6 +37,8 @@ export default function EventPage({ evt }) {
         <p>{evt.description}</p>
         <h3>Lieu : {evt.venue}</h3>
         <p>{evt.address}</p>
+
+        <EventMap evt={evt} />
 
         <Link href='/events'>
           <a className={styles.back}>{'<'} Retour</a>
